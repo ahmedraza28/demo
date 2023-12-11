30515d8e8493847ad10e73d6113b1a082e4d0861
ahmedraza28 <88032839+ahmedraza28@users.noreply.github.com>
2023-12-11 12:56:23 +0500

    added array to store coordinates

--- src/components/Stage.tsx
+++ src/components/Stage.tsx
@@ -1,16 +1,8 @@
-// Copyright (c) Meta Platforms, Inc. and affiliates.
-// All rights reserved.
-
-// This source code is licensed under the license found in the
-// LICENSE file in the root directory of this source tree.
-
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import * as _ from "underscore";
 import Tool from "./Tool";
 import { modelInputProps } from "./helpers/Interfaces";
 import AppContext from "./hooks/createContext";
-
-// ... (previous code)
 
 const Stage = () => {
   const {
@@ -18,15 +10,14 @@ const Stage = () => {
     image: [image],
   } = useContext(AppContext)!;
 
+  const [coordinates, setCoordinates] = useState<Array<[number, number]>>([]);
+
   const getClick = (x: number, y: number): modelInputProps => {
     const clickType = 1;
     return { x, y, clickType };
   };
 
-  // Get mouse position and scale the (x, y) coordinates back to the natural
-  // scale of the image. Update the state of clicks with setClicks to trigger
-  // the ONNX model to run and generate a new mask via a useEffect in App.tsx
-  const handleMouseMove = _.throttle((e: any) => {
+  const handleMouseClick = (e: any) => {
     let el = e.nativeEvent.target;
     const rect = el.getBoundingClientRect();
     let x = e.clientX - rect.left;
@@ -36,14 +27,26 @@ const Stage = () => {
     y *= imageScale;
     const click = getClick(x, y);
     if (click) {
-      setClicks([click]);
-      console.log(`Masking Coordinates: x=${x}, y=${y}`);
+      setCoordinates((prevCoordinates: [number, number][]) => {
+        const newCoordinates = [...prevCoordinates, [x, y]] as [number, number][];
+        setClicks([click]);
+        console.log(`Masking Coordinates: x=${x}, y=${y}`);
+        console.log("Current Coordinates Array:", newCoordinates);
+        return newCoordinates;
+      });
     }
+  };
+  
+  
+  
+
+  const handleMouseMove = _.throttle((e: any) => {
+    // Handle mouse move logic here...
   }, 15);
 
   const flexCenterClasses = "flex items-center justify-center";
   return (
-    <div className={`${flexCenterClasses} w-full h-full`}>
+    <div className={`${flexCenterClasses} w-full h-full`} onClick={handleMouseClick}>
       <div className={`${flexCenterClasses} relative w-[90%] h-[90%]`}>
         <Tool handleMouseMove={handleMouseMove} />
       </div>
